94719649b34d441990d412c1591581da58a1f858
Naoki Sugimoto <assoonasp_t@yahoo.co.jp>
2016-09-11 07:15:37 +0900

    preload

--- script/script.js
+++ script/script.js
@@ -1,7 +1,5 @@
 $(function() {
-    jQuery.preloadImages = function() {
-        jQuery("<img>").attr("src", "../image/led_on.png");
-    };
+    $("<img>").attr("src", "image/led_on.png");
     var pat = [0, 0, 0, 0, 0, 0, 0, 0];
 
     $('.led').addClass('off');
